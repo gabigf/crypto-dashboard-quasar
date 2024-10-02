491f143fe03bc76c27cc2988ce3ce777049d8886
Gabriela Guimaraes <gabriela.g.ferreira91@gmail.com>
2024-10-01 23:55:40 -0400

    Define currencies and exchange rate component

--- src/components/CurrencyConverter.vue
+++ src/components/CurrencyConverter.vue
@@ -2,45 +2,80 @@
   <q-card flat bordered class="my-card">
     <q-card-section>
       <div class="text-h6">Currency Converter</div>
+      <button>Clear all</button>
     </q-card-section>
 
-    <q-card-section class="q-pt-none">
-      <q-btn-dropdown color="primary" label="Dropdown Button">
-        <q-list>
-          <q-item clickable v-close-popup @click="onItemClick">
-            <q-item-section>
-              <q-item-label>Photos</q-item-label>
-            </q-item-section>
-          </q-item>
+    <q-card-section>
+      <q-input filled v-model="amount" type="number" input-class="text-right">
+        <template v-slot:before>
+          <q-btn-dropdown color="primary" :label="chosenPrimaryCurrency">
+            <q-list>
+              <q-item
+                v-for="(item, index) in currencies"
+                :key="index"
+                clickable
+                v-close-popup
+                @click="onItemClick"
+              >
+                <q-item-section>
+                  <q-item-label>{{ item }}</q-item-label>
+                </q-item-section>
+              </q-item>
+            </q-list>
+          </q-btn-dropdown>
+        </template>
+      </q-input>
 
-          <q-item clickable v-close-popup @click="onItemClick">
-            <q-item-section>
-              <q-item-label>Videos</q-item-label>
-            </q-item-section>
-          </q-item>
-
-          <q-item clickable v-close-popup @click="onItemClick">
-            <q-item-section>
-              <q-item-label>Articles</q-item-label>
-            </q-item-section>
-          </q-item>
-        </q-list>
-      </q-btn-dropdown>
+      <q-input filled v-model="result" type="number" input-class="text-right">
+        <template v-slot:before>
+          <q-btn-dropdown color="primary" :label="chosenSecondaryCurrency">
+            <q-list>
+              <q-item
+                v-for="(item, index) in currencies"
+                :key="index"
+                clickable
+                v-close-popup
+                @click="onItemClick"
+              >
+                <q-item-section>
+                  <q-item-label>{{ item }}</q-item-label>
+                </q-item-section>
+              </q-item>
+            </q-list>
+          </q-btn-dropdown>
+        </template>
+      </q-input>
     </q-card-section>
 
     <q-separator inset />
 
     <q-card-section>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua.
+      <ExchangeRate :exchange-data="exchangedData" />
     </q-card-section>
   </q-card>
 </template>
 
 <script setup>
+import { ref, reactive } from "vue";
+import ExchangeRate from "./ExchangeRate.vue";
+
 defineOptions({
   name: "CurrencyConverter",
 });
+
+const currencies = ["BTC", "ETH", "USD", "XRP", "LTC", "ADA"];
+const chosenPrimaryCurrency = ref("BTC");
+const chosenSecondaryCurrency = ref("BTC");
+const amount = ref(1);
+
+const exchangedData = reactive({
+  primaryCurrency: "BTC",
+  secondaryCurrency: "BTC",
+  exchangeRate: 0,
+});
+
+const result = ref(0);
+
 function onItemClick() {
   // console.log('Clicked on an Item')
 }
---
+++ src/components/ExchangeRate.vue
@@ -0,0 +1,14 @@
+<template>
+  <p>Exchange Rate</p>
+  <p>{{ props.exchangeData }}</p>
+</template>
+
+<script setup>
+defineOptions({
+  name: "ExchangeRate",
+});
+
+const props = defineProps({
+  exchangeData: Object,
+});
+</script>
